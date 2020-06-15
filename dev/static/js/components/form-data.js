4ac9b208f4b027ca5007c7ac7e6d99ecd6ac87d6
Your Name <you@example.com>
2020-06-15 16:39:44 +0300

    Cart Page

--- dev/static/js/components/form-data.js
+++ dev/static/js/components/form-data.js
@@ -12,16 +12,14 @@ class FormData {
                 error: 'Не дозвонимся по этому номеру и не сможем уточнить детали заказа '
             } ,
             mail: {
-                success: '',
+                success: 'Мы пришлём письмо с подтверждением заказа',
                 error: 'Не сможем отправить письмо с подтверждением заказа на этот адрес'
             }
         };
         this.validateReg = {
-            phone: /^\+375 [0-9]{2} [0-9]{3}-[0-9]{2}-[0-9]{2}$/,
+            phone: /^\+375 [0-9]{2} [0-9]{3} [0-9]{2} [0-9]{2}$/,
             mail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         };
-
-
     }
 
     init() {
@@ -55,11 +53,14 @@ class FormData {
 
             field.addEventListener('keyup', function() {
 
+
                 const validType = this.dataset.formValid;
                 const isValid = self.validate(this.value, validType);
                 const fieldErrorStr = 'form-data__field--error';
                 const field = this.closest('.form-data__field');
                 const errorField = field.querySelector('.form-data__error');
+
+
 
                 // If not validate
                 if(!isValid) {
