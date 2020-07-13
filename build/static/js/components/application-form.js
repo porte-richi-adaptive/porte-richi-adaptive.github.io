342e941705f27f3e6fb219be1e161ce4d0e4756b
Your Name <you@example.com>
2020-07-13 17:14:02 +0300

    app

--- build/static/js/components/application-form.js
+++ build/static/js/components/application-form.js
@@ -16,9 +16,11 @@ function () {
     this.fields = this.form.querySelectorAll('input[required], textarea[required]');
     this.submitBtns = this.form.querySelector(".".concat(formId, "__submit"));
     this.validateRegExp = {
+      text: /[\wа-я]+/ig,
       phone: /^\+ 375 \([0-9]{2}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$/,
       mail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     };
+    this.errorTextTip = this.form.querySelector(".".concat(formId, "__required"));
     this.init();
   }
 
@@ -26,6 +28,7 @@ function () {
     key: "init",
     value: function init() {
       this.setFieldEvents(this.fields);
+      this.setSubmitEvent(this.submitBtns);
     }
   }, {
     key: "setFieldEvents",
@@ -38,25 +41,143 @@ function () {
         switch (fieldType) {
           case 'tel':
             Inputmask({
-              "mask": "+ 375 (99) 999-99-99"
+              "mask": "+ 375 (99) 999-99-99",
+              showMaskOnHover: false
             }).mask(item);
 
             _this.phoneFieldEvent(item);
+
+            break;
+
+          case 'text':
+            _this.textFieldEvent(item);
 
             break;
         }
       });
     }
   }, {
-    key: "phoneFieldEvent",
-    value: function phoneFieldEvent(field) {
+    key: "changeSubmitState",
+    value: function changeSubmitState() {
+      var counter = 0;
+      this.fields.forEach(function (field, index, arr) {
+        if (field.classList.contains('success')) {
+          ++counter;
+        }
+      });
+
+      if (counter === this.fields.length) {
+        this.submitBtns.classList.remove('disabled');
+      } else {
+        this.submitBtns.classList.add('disabled');
+      }
+    }
+  }, {
+    key: "textFieldEvent",
+    value: function textFieldEvent(field) {
       var _this2 = this;
 
-      field.addEventListener('keyup', function (e) {
-        var val = _this2.validateRegExp['phone'].test(field.value);
+      var isValid = this.validateRegExp['text'].test(field.value);
 
-        if (val) {
-          console.dir(true);
+      if (isValid) {
+        this.setSuccessState(field);
+      } else {
+        this.setErrorState(field);
+      }
+
+      field.addEventListener('keyup', function () {
+        var isValid = _this2.validateRegExp['text'].test(field.value);
+
+        field.dataset.validState = '';
+
+        _this2.disableRequiredTips();
+
+        if (isValid) {
+          _this2.setSuccessState(field);
+        } else {
+          _this2.setErrorState(field);
+        }
+
+        _this2.changeSubmitState();
+      });
+    }
+  }, {
+    key: "activateRequiredTips",
+    value: function activateRequiredTips() {
+      this.errorTextTip.classList.add('active');
+    }
+  }, {
+    key: "disableRequiredTips",
+    value: function disableRequiredTips() {
+      this.errorTextTip.classList.remove('active');
+    }
+  }, {
+    key: "setSubmitEvent",
+    value: function setSubmitEvent(btn) {
+      var _this3 = this;
+
+      btn.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        _this3.fields.forEach(function (field, index, arr) {
+          if (field.classList.contains('error')) {
+            field.dataset.validState = 'error';
+
+            _this3.activateRequiredTips();
+          }
+        });
+      });
+    }
+  }, {
+    key: "setSuccessState",
+    value: function setSuccessState(field) {
+      field.classList.remove('error');
+      field.dataset.validState = '';
+      field.classList.add('success');
+    }
+  }, {
+    key: "setErrorState",
+    value: function setErrorState(field) {
+      field.classList.remove('success');
+      field.classList.add('error');
+    }
+  }, {
+    key: "phoneFieldEvent",
+    value: function phoneFieldEvent(field) {
+      var _this4 = this;
+
+      if (this.validateRegExp['phone'].test(field.value)) {
+        this.setSuccessState(field);
+      } else {
+        this.setErrorState(field);
+      }
+
+      field.addEventListener('keyup', function (e) {
+        var isValid = _this4.validateRegExp['phone'].test(field.value);
+
+        field.dataset.validState = '';
+
+        _this4.disableRequiredTips();
+
+        if (isValid) {
+          _this4.setSuccessState(field);
+        } else {
+          _this4.setErrorState(field);
+        }
+
+        _this4.changeSubmitState();
+      });
+      field.addEventListener('blur', function () {
+        var isValid = _this4.validateRegExp['phone'].test(field.value);
+
+        if (isValid) {
+          _this4.setSuccessState(field);
+
+          field.dataset.validState = '';
+        } else {
+          _this4.setErrorState(field);
+
+          field.dataset.validState = 'error';
         }
       });
     }
@@ -66,4 +187,3 @@ function () {
 }();
 
 var app = new FormCreator('application-form');
-console.dir(app);
--- dev/static/js/components/application-form.js
+++ dev/static/js/components/application-form.js
@@ -4,36 +4,142 @@ class FormCreator {
         this.fields = this.form.querySelectorAll('input[required], textarea[required]');
         this.submitBtns = this.form.querySelector(`.${formId}__submit`);
         this.validateRegExp = {
+            text: /[\wа-я]+/ig,
             phone: /^\+ 375 \([0-9]{2}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$/,
             mail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         };
+        this.errorTextTip = this.form.querySelector(`.${formId}__required`);
         this.init();
     }
     init() {
         this.setFieldEvents(this.fields);
-
+        this.setSubmitEvent(this.submitBtns);
     }
     setFieldEvents(fields) {
         fields.forEach((item, index, arr) => {
             const fieldType = item.type;
             switch(fieldType) {
                 case 'tel':
-                    Inputmask({"mask": "+ 375 (99) 999-99-99"}).mask(item);
+                    Inputmask({"mask": "+ 375 (99) 999-99-99", showMaskOnHover: false,}).mask(item);
                     this.phoneFieldEvent(item);
+                    break;
+                case 'text':
+                    this.textFieldEvent(item);
                     break;
             }
         });
     }
 
-    phoneFieldEvent(field) {
-        field.addEventListener('keyup', (e) => {
-            let val = this.validateRegExp['phone'].test(field.value);
-            if(val) {
-                console.dir(true);
+    changeSubmitState() {
+        let counter = 0;
+        this.fields.forEach((field, index, arr) => {
+            if( field.classList.contains('success') ){
+                ++counter;
             }
+        });
+        if(counter === this.fields.length) {
+            this.submitBtns.classList.remove('disabled');
+        } else {
+            this.submitBtns.classList.add('disabled');
+        }
+
+    }
+
+    textFieldEvent(field) {
+        let isValid = this.validateRegExp['text'].test(field.value);
+        if(isValid) {
+            this.setSuccessState(field);
+        } else {
+            this.setErrorState(field);
+        }
+
+        field.addEventListener('keyup', () => {
+            let isValid = this.validateRegExp['text'].test(field.value);
+            field.dataset.validState = '';
+            this.disableRequiredTips();
+
+
+            if(isValid) {
+                this.setSuccessState(field);
+            } else {
+                this.setErrorState(field);
+            }
+            this.changeSubmitState();
+        });
+    }
+
+    activateRequiredTips() {
+        this.errorTextTip.classList.add('active');
+    }
+
+    disableRequiredTips() {
+        this.errorTextTip.classList.remove('active');
+    }
+
+    setSubmitEvent(btn) {
+
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            this.fields.forEach((field, index, arr) => {
+                if(field.classList.contains('error')) {
+                    field.dataset.validState = 'error';
+                    this.activateRequiredTips();
+                }
+            });
+
+        });
+    }
+
+
+    setSuccessState(field) {
+        field.classList.remove('error');
+        field.dataset.validState = '';
+        field.classList.add('success');
+    }
+
+    setErrorState(field) {
+        field.classList.remove('success');
+        field.classList.add('error');
+    }
+
+    phoneFieldEvent(field) {
+
+        if(this.validateRegExp['phone'].test(field.value)) {
+            this.setSuccessState(field);
+        } else {
+            this.setErrorState(field);
+        }
+
+        field.addEventListener('keyup', (e) => {
+            let isValid = this.validateRegExp['phone'].test(field.value);
+
+            field.dataset.validState = '';
+            this.disableRequiredTips();
+
+            if(isValid) {
+                this.setSuccessState(field);
+
+            } else {
+                this.setErrorState(field);
+            }
+            this.changeSubmitState();
+        });
+
+        field.addEventListener('blur', () => {
+            let isValid = this.validateRegExp['phone'].test(field.value);
+
+            if(isValid) {
+                this.setSuccessState(field);
+                field.dataset.validState = '';
+
+            } else {
+                this.setErrorState(field);
+                field.dataset.validState = 'error';
+            }
+
         });
     }
 }
 
 const app = new FormCreator('application-form');
-console.dir(app);
