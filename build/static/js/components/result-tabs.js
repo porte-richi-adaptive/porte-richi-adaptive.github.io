9e88410fcf4791c2741bfeff8592322f3cba3c18
Your Name <you@example.com>
2020-07-15 15:29:33 +0300

    app

---
+++ build/static/js/components/result-tabs.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var state = {
+  'disable': 'result-tabs__tab--disabled'
+};
+var container = document.querySelector('.result-tabs');
+var tabs = container.querySelectorAll('.result-tabs__tab');
+var pages = container.querySelectorAll('.result-tabs__page');
+tabs.forEach(function (tab, i, arr) {
+  tab.addEventListener('click', function (e) {
+    e.preventDefault();
+    var name = tab.dataset.tab || '';
+    var page = container.querySelector("[data-tabs-page=".concat(name, "]"));
+    addDisableState(tabs);
+    closeAllPages(pages);
+    page.style.display = 'block';
+    this.classList.remove(state['disable']);
+  });
+});
+
+function addDisableState(items) {
+  items.forEach(function (item) {
+    item.classList.add(state['disable']);
+  });
+}
+
+function closeAllPages(pages) {
+  pages.forEach(function (page) {
+    page.style.display = 'none';
+  });
+}
---
+++ dev/static/js/components/result-tabs.js
@@ -0,0 +1,36 @@
+const state = {
+    'disable': 'result-tabs__tab--disabled'
+};
+const container = document.querySelector('.result-tabs');
+const tabs = container.querySelectorAll('.result-tabs__tab');
+const pages = container.querySelectorAll('.result-tabs__page');
+
+
+tabs.forEach((tab, i, arr) => {
+
+    tab.addEventListener('click', function(e) {
+        e.preventDefault();
+        const name = tab.dataset.tab || '';
+        const page = container.querySelector(`[data-tabs-page=${name}]`);
+
+        addDisableState(tabs);
+        closeAllPages(pages);
+
+        page.style.display = 'block';
+
+
+        this.classList.remove(state['disable']);
+    });
+});
+
+function addDisableState(items) {
+    items.forEach((item) => {
+        item.classList.add(state['disable']);
+    });
+}
+
+function closeAllPages(pages) {
+    pages.forEach((page) => {
+        page.style.display = 'none';
+    });
+}
