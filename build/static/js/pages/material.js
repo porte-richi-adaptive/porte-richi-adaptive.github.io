bf5cb212cbd1faeb6b155ed90dc3e4dfe9a4ef0d
Your Name <you@example.com>
2020-08-26 17:17:23 +0300

    app

--- build/static/js/pages/material.js
+++ build/static/js/pages/material.js
@@ -2,6 +2,8 @@
 
 // Veneer Tabs
 function tabInit() {
+  var list = $('.material__veneer-list');
+  var dropdown = $('[data-tab-dropdown]');
   $('.material__veneer-item').removeClass('active');
   $('.material__veneer-content').removeClass('active');
   $('.material__veneer-item[data-tab="1"]').addClass('active');
@@ -11,11 +13,22 @@ function tabInit() {
     var allTabs = $('.material__veneer-item[data-tab]');
     var allPage = $('.material__veneer-content[data-page]');
     var pageActive = $(".material__veneer-content[data-page=".concat(tabNum, "]"));
+    var name = $(this).text();
+    dropdown.removeClass('opened');
+    dropdown.text(name);
     allTabs.removeClass('active');
     $(this).addClass('active');
     allPage.fadeOut(0);
     pageActive.fadeIn();
     pageActive.css('display', 'flex');
+  });
+  $('[data-tab-dropdown]').on('click', function () {
+    if ($(this).hasClass('opened')) {
+      $(this).removeClass('opened');
+      return true;
+    }
+
+    $(this).addClass('opened');
   });
 }
 
--- dev/static/js/pages/material.js
+++ dev/static/js/pages/material.js
@@ -1,6 +1,9 @@
 // Veneer Tabs
 function tabInit() {
 
+    const list = $('.material__veneer-list');
+    const dropdown = $('[data-tab-dropdown]');
+
     $('.material__veneer-item').removeClass('active');
     $('.material__veneer-content').removeClass('active');
 
@@ -13,6 +16,10 @@ function tabInit() {
         const allTabs = $('.material__veneer-item[data-tab]');
         const allPage = $('.material__veneer-content[data-page]');
         const pageActive = $(`.material__veneer-content[data-page=${tabNum}]`);
+        const name = $(this).text();
+
+        dropdown.removeClass('opened');
+        dropdown.text(name);
 
         allTabs.removeClass('active');
         $(this).addClass('active');
@@ -20,6 +27,14 @@ function tabInit() {
         allPage.fadeOut(0);
         pageActive.fadeIn();
         pageActive.css('display', 'flex');
+    });
+
+    $('[data-tab-dropdown]').on('click', function() {
+        if($(this).hasClass('opened')) {
+            $(this).removeClass('opened');
+            return true;
+        }
+        $(this).addClass('opened');
     });
 
 
