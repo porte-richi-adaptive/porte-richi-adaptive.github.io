8f30035b798d27758be9aa25e3e6e0bea5f67a4b
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-10-26 17:45:29 +0300

    init: desktop page

--- build/static/js/components/modal-builder.js
+++ build/static/js/components/modal-builder.js
@@ -10,9 +10,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 var prevArrowIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"complex__slider-prev complex__slider-arrow\" width=\"32\" height=\"62\" viewBox=\"0 0 32 62\" fill=\"none\"><path d=\"M31 61L1 31 31 1\" style=\"stroke-linejoin:round;stroke-width:2;\"/></svg>";
 var nextArrowIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\"  class=\"complex__slider-next complex__slider-arrow\" width=\"32\" height=\"62\" viewBox=\"0 0 32 62\" fill=\"none\"><path d=\"M1 61L31 31 1 1\" style=\"stroke-linejoin:round;stroke-width:2;\"/></svg>";
 
-var Modal =
-/*#__PURE__*/
-function () {
+var Modal = /*#__PURE__*/function () {
   function Modal() {
     _classCallCheck(this, Modal);
 
