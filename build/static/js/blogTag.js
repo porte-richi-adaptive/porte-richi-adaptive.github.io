2082691f0a1c96fea65cea916f2513a72da2d3f5
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-10-28 09:58:47 +0300

    add: tag table

--- build/static/js/blogTag.js
+++ build/static/js/blogTag.js
@@ -3,11 +3,14 @@
 var tagMenu = document.querySelector('.blog__tags__menu');
 var openTagMenu = document.querySelector('.blog__tags__open-menu');
 var crossButton = document.querySelector('.blog__tags__open-menu-top img');
+var blogTitle = document.querySelector('.blog__title');
 tagMenu.addEventListener('click', function () {
   tagMenu.style.display = "none";
+  blogTitle.style.display = "none";
   openTagMenu.style.display = "block";
 });
 crossButton.addEventListener('click', function () {
   tagMenu.style.display = "flex";
   openTagMenu.style.display = "none";
+  blogTitle.style.display = "block";
 });
--- dev/static/js/blogTag.js
+++ dev/static/js/blogTag.js
@@ -1,13 +1,16 @@
 const tagMenu = document.querySelector('.blog__tags__menu');
 const openTagMenu = document.querySelector('.blog__tags__open-menu');
 const crossButton = document.querySelector('.blog__tags__open-menu-top img');
+const blogTitle = document.querySelector('.blog__title');
 
 tagMenu.addEventListener('click', ()=>{
     tagMenu.style.display = "none";
+    blogTitle.style.display = "none"
     openTagMenu.style.display = "block";
 });
 
 crossButton.addEventListener('click', ()=>{
     tagMenu.style.display = "flex";
     openTagMenu.style.display = "none";
+    blogTitle.style.display = "block"
 });
